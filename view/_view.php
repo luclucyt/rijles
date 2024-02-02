37f3a6a1d07a5c1d9d7f89eddc21ed9e264a5ec5
luclucyt <vanbriemenlucas@gmail.com>
2024-02-02 13:18:08 +0100

    commit

--- view/_view.php
+++ view/_view.php
@@ -5,7 +5,6 @@ if (str_contains($_SERVER['REQUEST_URI'], 'view/')) {
     //get the url
     $url = $_SERVER['REQUEST_URI'];
 
-    //remove the view/ from the url
     $url = str_replace('view/', '', $url);
     $url = str_replace('.view', '', $url);
 
