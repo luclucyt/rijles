6afeb4e55e795b62dbcf8582bfa84dbf00e46fb6
luclucyt <vanbriemenlucas@gmail.com>
2024-02-03 13:18:34 +0100

    commit

--- view/_view.php
+++ view/_view.php
@@ -2,7 +2,6 @@
 
 //if the url contains a view/ then we know we are in the view folder
 if (str_contains($_SERVER['REQUEST_URI'], 'view/')) {
-    //get the url
     $url = $_SERVER['REQUEST_URI'];
 
     $url = str_replace('view/', '', $url);
