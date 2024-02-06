bbce11a7d10968fc344081b96dd51838ab8c75b9
luclucyt <vanbriemenlucas@gmail.com>
2024-02-06 13:20:15 +0100

    commit

--- view/_view.php
+++ view/_view.php
@@ -7,6 +7,6 @@ if (str_contains($_SERVER['REQUEST_URI'], 'view/')) {
     $url = str_replace('view/', '', $url);
     $url = str_replace('.view', '', $url);
 
-    //redirect to the new url (without 'view/' and '.view' in the url)
+    //redirect to the new url
     header('Location: ' . $url);
 }
